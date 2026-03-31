26ab0468116eff5ec1ac58d4465e9178fb954e7a
Brad Wilson <dotnetguy@gmail.com>
2026-03-31 15:32:45 -0700

    Combine [ClassData] and [ClassData<>] generators into the same source file

--- src/xunit.v3.core.aot.generators/Generators/ClassDataAttributeGenerator.cs
+++ src/xunit.v3.core.aot.generators/Generators/ClassDataAttributeGenerator.cs
@@ -22,3 +22,24 @@ public class ClassDataAttributeGenerator() :
 		ProcessClassDataAttribute(classSymbol, methodSymbol, attribute, classDataType, dataAttributeRegistration, result);
 	}
 }
+
+[Generator(LanguageNames.CSharp)]
+public class ClassDataAttributeOfTGenerator() :
+	ClassDataAttributeGeneratorBase(Types.Xunit.ClassDataAttribute + "`1")
+{
+	protected override void ProcessAttribute(
+		INamedTypeSymbol classSymbol,
+		IMethodSymbol methodSymbol,
+		AttributeData attribute,
+		string dataAttributeRegistration,
+		GeneratorResult result,
+		CancellationToken cancellationToken)
+	{
+		Guard.ArgumentNotNull(attribute);
+
+		if (attribute.AttributeClass?.TypeArguments.Length < 1 || attribute.AttributeClass?.TypeArguments[0] is not INamedTypeSymbol classDataType)
+			return;
+
+		ProcessClassDataAttribute(classSymbol, methodSymbol, attribute, classDataType, dataAttributeRegistration, result);
+	}
+}
